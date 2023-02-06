34f85da8f9edc71df7cd3a90470fc6a46e8eb389
Fernando Paulino <xizes2@hotmail.com>
2023-02-06 15:34:01 +0100

    add values interface, form state controller, delete non used features

--- src/hooks/useMultistepForm.ts
+++ src/hooks/useMultistepForm.ts
@@ -1,35 +1,50 @@
 import { ReactElement, useState } from "react";
 
+export interface IInitialValues {
+  name: string;
+  surname: string;
+  email: string;
+  userName: string;
+  password: string;
+  country: string;
+  address: string;
+}
+
 const useMultistepForm = (steps: ReactElement[]) => {
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
+  const [currentFormValues, setCurrentFormValues] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    userName: "",
+    password: "",
+    country: "",
+    address: "",
+  });
 
-  const previousStep = () => {
+  const previousStep = (values: IInitialValues) => {
+    setCurrentFormValues(values);
     setCurrentStepIndex((index) => {
       if (index <= 0) return index;
       return index - 1;
     });
   };
 
-  const nextStep = () => {
+  const nextStep = (values: IInitialValues) => {
+    setCurrentFormValues(values);
     setCurrentStepIndex((index) => {
       if (index >= steps.length - 1) return index;
       return index + 1;
     });
   };
 
-  const goToStep = (index: number) => {
-    setCurrentStepIndex(index);
-  };
-
   return {
-    steps,
-    step: steps[currentStepIndex],
     currentStepIndex,
+    currentFormValues,
     isFirstStep: currentStepIndex === 0,
     isLastStep: currentStepIndex === steps.length - 1,
     previousStep,
     nextStep,
-    goToStep,
   };
 };
 
