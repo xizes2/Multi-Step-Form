e3b1223ed16b3516e3756bb7da91fdd6b8a3412c
Fernando Paulino <xizes2@hotmail.com>
2023-02-15 15:47:07 +0100

    create Header and it's test

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="sticky top-0 flex min-w-[100vw] bg-slate-800/40 backdrop-blur-sm">
+      <h1 className="p-5 text-2xl text-white ">MultiStep Form</h1>
+    </div>
+  );
+};
+
+export default Header;
